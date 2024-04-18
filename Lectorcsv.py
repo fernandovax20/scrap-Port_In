11589904e949c2f825a2746bc5aec6505da0acdf
fernandovax20 <fernando.vax20@gmail.com>
2024-04-18 00:34:42 -0400

    Fix the test_v2.py to extract 12k data with headless

--- Lectorcsv.py
+++ Lectorcsv.py
@@ -1,12 +1,11 @@
 import pandas as pd
 
 # Reemplaza 'ruta_del_archivo.csv' con la ruta real de tu archivo
-archivo = 'output_data.csv'
-df = pd.read_csv(archivo, delimiter='|', nrows=0)  # nrows=0 carga solo las cabeceras
+archivo = 'Ds_output/output_data.csv'
+df = pd.read_csv(archivo, delimiter='|')  # Carga todo el DataFrame sin el parámetro nrows
 
 # Mostrar las cabeceras originales
 original_headers = df.columns.tolist()
-original_headers
 
 def limpiar_cabecera(cabecera):
     sufijos = ['_tmin', '_tmax', '_tavg', '_dmin', '_dmax', '_davg']
@@ -24,7 +23,19 @@ cabeceras_unicas = list(set(cabeceras_limpias))
 cabeceras_unicas.sort()
 
 # Mostrar las cabeceras únicas y contar cuántas son
-cabeceras_unicas, len(cabeceras_unicas)
+print(f"{len(cabeceras_unicas)} cabeceras únicas:", cabeceras_unicas)
 
-print(len(cabeceras_unicas), "cabeceras únicas")
-print(cabeceras_unicas)
+# Contar el número de filas en el dataset
+numero_de_filas = df.shape[0]
+
+# Imprimir los resultados
+print(f"Número de filas en el dataset: {numero_de_filas}")
+
+
+columna_primera = df.columns[0]  # Asumiendo que es la primera columna
+
+# Contar cuántas filas tienen la primera columna vacía o nula
+filas_con_primera_columna_vacia = df[columna_primera].isna().sum()
+
+# Imprimir el resultado
+print(f"Número de filas con la primera columna ({columna_primera}) vacía o nula: {filas_con_primera_columna_vacia}")
