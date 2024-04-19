3b97ac04474db15767326132fe09085707536057
fernandovax20 <fernando.vax20@gmail.com>
2024-04-19 16:26:10 -0400

    Clean the repo

--- merge_csv.py
+++ merge_csv.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import os
 
-def merge_csv_files(input_folder, output_file, delimiter='|'):
+def merge_and_clean_csv_files(input_folder, output_file, delimiter='|'):
     dataframes = []
     for filename in os.listdir(input_folder):
         if filename.endswith('.csv'):
@@ -15,14 +15,20 @@ def merge_csv_files(input_folder, output_file, delimiter='|'):
     
     # Eliminar duplicados basándonos en la columna 'url'
     combined_df = combined_df.drop_duplicates(subset=['url'])
-    
-    # Guardar el dataframe combinado en un nuevo archivo CSV
-    combined_df.to_csv(output_file, index=False, sep=delimiter)
-    print(f'Archivo combinado guardado en: {output_file}')
+
+    # Columnas a verificar por valores nulos
+    columns_to_check = ['direccion', 'precio_uf', 'metros_cuadrados', 'dormitorios', 'banios', 'latitude', 'longitude']
+
+    # Filtrar filas que no tengan nulos en 'columns_to_check'
+    clean_df = combined_df[~combined_df[columns_to_check].isnull().any(axis=1)]
+
+    # Guardar el DataFrame limpio en un nuevo archivo CSV
+    clean_df.to_csv(output_file, index=False, sep=delimiter)
+    print(f'Archivo combinado y limpio guardado en: {output_file}')
 
 # Configuración de rutas de archivo
 input_folder_path = 'Ds_Output'  # Asegúrate de que esta ruta sea correcta
-output_csv_path = 'Ds_Output/combined_output.csv'  # Ajusta esta ruta y nombre de archivo como prefieras
+output_csv_path = 'Ds_Output/combined_clean_output.csv'  # Ajusta esta ruta y nombre de archivo como prefieras
 
 # Llamar a la función con las rutas de los archivos
-merge_csv_files(input_folder_path, output_csv_path)
+merge_and_clean_csv_files(input_folder_path, output_csv_path)
